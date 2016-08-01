c752959e0d04291c1f79673f3395febe382b4ea9
Luca Zecca <l.zecca78@gmail.com>
2016-08-01 09:06:40 +0200

    speedup ctrl-p on git projects

--- scripts/install_vim_plugins.sh
+++ scripts/install_vim_plugins.sh
@@ -129,6 +129,19 @@ if !empty(glob("/usr/bin/python3"))
 let g:syntastic_python_python_exec = '/usr/bin/python3'
 endif
 
+"speedup Ctrl-P on git projects
+let g:ctrlp_use_caching = 0
+if executable('ag')
+    set grepprg=ag\ --nogroup\ --nocolor
+
+    let g:ctrlp_user_command = 'ag %s -l --nocolor -g ""'
+else
+  let g:ctrlp_user_command = ['.git', 'cd %s && git ls-files . -co --exclude-standard', 'find %s -type f']
+  let g:ctrlp_prompt_mappings = {
+    \ 'AcceptSelection("e")': ['<space>', '<cr>', '<2-LeftMouse>'],
+    \ }
+endif
+
 
 END
 
