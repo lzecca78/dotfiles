cadcf097796f9dd40b0eb849f31fc22bed36fc03
Luca Zecca <l.zecca78@gmail.com>
2018-08-30 14:41:33 +0200

    added Ark utility for Heptio backup and refactored code

--- scripts/install_k8s_utils.sh
+++ scripts/install_k8s_utils.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+###vars
+kustomize_version='1.0.7'
+ark_version='0.9.3'
+
+
+###functions
+function install_with_wget() {
+    local_path=$1
+    url=$2
+    wget -O $local_path $url && chmod +x $local_path
+}
 
 function add_aliases_mac() {
     grep -q -F 'source ~/.k8s_aliases' ~/.zshrc || echo 'source ~/.k8s_aliases' >> ~/.zshrc
@@ -19,17 +30,27 @@ alias ksn='kubens'
 EOF
 }
 
+function completion_for_aliases() {
+    grep -q -F '# Uncomment and edit these lines to add your own alias completions' ~/.bash_completion ||  wget https://raw.githubusercontent.com/cykerway/complete-alias/master/completions/bash_completion.sh -O ->> ~/.bash_completion
+    grep -q -F 'complete -F _complete_alias kl' ~/.bash_completion || echo 'complete -F _complete_alias kl' >> ~/.bash_completion
+    grep -q -F 'complete -F _complete_alias kt' ~/.bash_completion || echo 'complete -F _complete_alias kt' >> ~/.bash_completion
+    grep -q -F 'complete -F _complete_alias ksn' ~/.bash_completion || echo 'complete -F _complete_alias ksn' >> ~/.bash_completion
+}
 
 function install_linux() {
     echo "installing kubetail utilities.... please wait"
-        wget -O /usr/local/bin/kubetail https://raw.githubusercontent.com/johanhaleby/kubetail/master/kubetail && chmod +x /usr/local/bin/kubetail
-        wget -O /etc/bash_completion.d/kubetail.bash  https://raw.githubusercontent.com/johanhaleby/kubetail/master/completion/kubetail.bash
+        install_with_wget /usr/local/bin/kubetail https://raw.githubusercontent.com/johanhaleby/kubetail/master/kubetail
+        install_with_wget /etc/bash_completion.d/kubetail.bash https://raw.githubusercontent.com/johanhaleby/kubetail/master/completion/kubetail.bash
     echo "installing kubectx utility... please wait"
-        wget -O /usr/local/bin/kubectx https://raw.githubusercontent.com/ahmetb/kubectx/master/kubectx && chmod +x /usr/local/bin/kubectx
-        wget -O /etc/bash_completion.d/kubectx.bash https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubectx.bash
+        install_with_wget /usr/local/bin/kubectx https://raw.githubusercontent.com/ahmetb/kubectx/master/kubectx
+        install_with_wget /etc/bash_completion.d/kubectx.bash https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubectx.bash
     echo "installing kubens utility... please wait"
-        wget -O /usr/local/bin/kubens https://raw.githubusercontent.com/ahmetb/kubectx/master/kubens && chmod +x /usr/local/bin/kubens
-        wget -O /etc/bash_completion.d/kubens.bash https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubens.bash
+        install_with_wget /usr/local/bin/kubens https://raw.githubusercontent.com/ahmetb/kubectx/master/kubens
+        install_with_wget /etc/bash_completion.d/kubens.bash https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubens.bash
+    echo "installing kustomize utility.. please wait"
+        install_with_wget /usr/local/bin/kustomize https://github.com/kubernetes-sigs/kustomize/releases/download/v${kustomize_version}/kustomize_${kustomize_version}_linux_amd64
+    echo "installing Heptio ark backup cli utility... please wait"
+         wget -qO- https://github.com/heptio/ark/releases/download/v${ark_version}/ark-v${ark_version}-linux-amd64.tar.gz | tar xvz -C /usr/local/bin
 }
 
 function install_mac() {
@@ -37,14 +58,19 @@ function install_mac() {
         brew install kubectx
     echo "installing kubetail utilities.. please wait"
         brew tap johanhaleby/kubetail && brew install kubetail
+    echo "installing kustomize utility.. please wait"
+        install_with_wget /usr/local/bin/kustomize https://github.com/kubernetes-sigs/kustomize/releases/download/v${kustomize_version}/kustomize_${kustomize_version}_darwin_amd64
+    echo "installing Heptio ark backup cli utility... please wait"
+         wget -qO- https://github.com/heptio/ark/releases/download/v${ark_version}/ark-v${ark_version}-darwin-amd64.tar.gz | tar xvz -C /usr/local/bin
 }
 
 
-unameOut="$(uname -s)"
+###execution
 case "$OSTYPE" in
-    linux*)  
+    linux*)
         install_linux
         add_aliases_linux
+        completion_for_aliases
        ;;
     darwin*)
         install_mac
@@ -54,4 +80,3 @@ case "$OSTYPE" in
         echo "unknown OSTYPE = ${OSTYPE}"
        ;;
 esac
-
