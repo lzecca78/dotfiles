71e61f1d2dcbcf85a342bfdf88de0ef915a07532
Luca Zecca <l.zecca78@gmail.com>
2019-06-24 08:52:25 +0200

    added autocompletion of function in Go

--- scripts/install_vim_plugins.sh
+++ scripts/install_vim_plugins.sh
@@ -147,6 +147,7 @@ let g:go_highlight_function_calls = 1
 let g:go_highlight_operators = 1
 let g:go_highlight_extra_types = 1
 let g:go_highlight_build_constraints = 1
+au filetype go inoremap <buffer> . .<C-x><C-o>
 autocmd BufNewFile,BufRead *.go setlocal noexpandtab tabstop=4 shiftwidth=4
 let g:go_auto_sameids = 1
 let g:rehash256 = 1
