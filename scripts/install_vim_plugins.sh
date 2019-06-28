4898e3a3a2c1c8ee50debd48cc54566415ebbd49
Luca Zecca <l.zecca78@gmail.com>
2019-06-28 14:33:59 +0200

    Update install_vim_plugins.sh

--- scripts/install_vim_plugins.sh
+++ scripts/install_vim_plugins.sh
@@ -147,6 +147,7 @@ let g:go_highlight_function_calls = 1
 let g:go_highlight_operators = 1
 let g:go_highlight_extra_types = 1
 let g:go_highlight_build_constraints = 1
+let g:go_def_mode='godef'
 au filetype go inoremap <buffer> . .<C-x><C-o>
 autocmd BufNewFile,BufRead *.go setlocal noexpandtab tabstop=4 shiftwidth=4
 let g:go_auto_sameids = 1
